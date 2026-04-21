58ee6a948940afa3696c51d35b59ab3f799bfd84
Muhammad Irfan <mrfansi@outlook.com>
2026-04-22 01:55:46 +0700

    feat(waba): add AssignRequestId middleware

---
+++ app/Http/Middleware/AssignRequestId.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
+
+class AssignRequestId
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $id = $request->headers->get('X-Request-Id') ?: (string) Str::ulid();
+        $request->attributes->set('request_id', $id);
+
+        Log::withContext(['request_id' => $id]);
+
+        /** @var Response $response */
+        $response = $next($request);
+        $response->headers->set('X-Request-Id', $id);
+
+        return $response;
+    }
+}
